22a0191f58e7b28ae3d0cc98d779af28b7fe7cde
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2023-12-31 22:33:50 -0300

    ADDED: Reading part of the file. Closes #3

--- 1 - How to read files/ReadFiles/Program.cs
+++ 1 - How to read files/ReadFiles/Program.cs
@@ -2,11 +2,13 @@ var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 var filePath = Path.Combine(docPath, "Projects\\Studies\\C#\\uncomplicating-the-csharp\\1 - How to read files\\text-file.txt");
 
 
-Console.WriteLine("\n==== Read AllLines ====");
-var lines = File.ReadAllLines(filePath);
-int count = 0;
-foreach (var line in lines)
-{
-    count++;
-    Console.WriteLine($"Line: {count} - {line}");
-}
+//read part of the file
+//recommended for large files
+Console.WriteLine("\n==== StreamReader ====");
+using var file = new StreamReader(filePath);
+string? line;
+
+while ((line = file.ReadLine()) != null)
+    Console.WriteLine(line);
+
+file.Close();
