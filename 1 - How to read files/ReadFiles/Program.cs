4c3eceed4d160741a7668ed3f1f53078eb7520de
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2023-12-31 22:17:10 -0300

    ADDED: Reading a file line by line. Closes #2

--- 1 - How to read files/ReadFiles/Program.cs
+++ 1 - How to read files/ReadFiles/Program.cs
@@ -1,7 +1,12 @@
 var docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 var filePath = Path.Combine(docPath, "Projects\\Studies\\C#\\uncomplicating-the-csharp\\1 - How to read files\\text-file.txt");
 
-var allText = File.ReadAllText(filePath);
-Console.WriteLine("==== Read AllText ====");
-Console.WriteLine(allText);
 
+Console.WriteLine("\n==== Read AllLines ====");
+var lines = File.ReadAllLines(filePath);
+int count = 0;
+foreach (var line in lines)
+{
+    count++;
+    Console.WriteLine($"Line: {count} - {line}");
+}
