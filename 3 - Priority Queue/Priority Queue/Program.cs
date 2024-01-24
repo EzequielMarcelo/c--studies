44306fe63f551962967adbb7e936a8c6cc834b9a
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-01-24 19:54:50 -0300

    Closes #8: Enqueue and dequeue items.

---
+++ 3 - Priority Queue/Priority Queue/Program.cs
@@ -0,0 +1,9 @@
+﻿var queue = new PriorityQueue<string, int>();
+
+queue.Enqueue("Item 1", 0);
+queue.Enqueue("Item 2", 4);
+queue.Enqueue("Item 3", 2);
+queue.Enqueue("Item 4", 1);
+
+while(queue.TryDequeue(out string? item, out int priority))
+    Console.WriteLine($"Item: {item} Priority: {priority}");
